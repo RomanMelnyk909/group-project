37242d0b6729d94b314f2af8ff0bac69c273bdc4
RomanMelnyk909 <45914004+RomanMelnyk909@users.noreply.github.com>
2023-12-06 19:16:59 +0200

    added basic layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,194 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PageWrapper from "./components/PageWrapper";
+
+const lor = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad deserunt
+veritatis excepturi dolores adipisci, quae facere sapiente odio saepe
+voluptate, officiis voluptatibus sequi minima vel labore numquam maxime
+enim esse.`;
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <PageWrapper>{lor}</PageWrapper>
       <Footer />
     </div>
   );
