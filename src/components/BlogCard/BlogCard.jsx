74868545808edc4c3fd9c2bdb69edfc98e1f1a6b
DrobenyukA <drobenyuk.a@gmail.com>
2023-12-07 20:22:34 +0200

    add BlogCard

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,28 +1,72 @@
 import React, { useState } from "react";
 import Button from "./Button";
 
-const BlogCard = ({ name, text, image, isShow, dateTimePublish, onAdd, onDelete }) => {
+import styles from './blogCard.module.css'
+
+const BlogCard = ({ 
+    name: initialName, 
+    text: initialText, 
+    isShow: initialIsShow, 
+    dateTimePublish: initialDateTimePublish, 
+    onAdd, onDelete }) => {
+        
+  const [name, setName] = useState(initialName);
+  const [text, setText] = useState(initialText);
+  const [isShow, setIsShow] = useState(initialIsShow);
+  const [dateTimePublish, setDateTimePublish] = useState(initialDateTimePublish);
   const [showDetails, setShowDetails] = useState(false);
 
   const handleToggleDetails = () => {
     setShowDetails(!showDetails);
   };
+    const handleAddCard = () => {
+    // Умови додавання картки
+    if (name && text && dateTimePublish) {
+      onAdd({ name, text, isShow, dateTimePublish });
+      // Очистити поля 
+      setName("");
+      setText("");
+      setIsShow(false);
+      setDateTimePublish("");
+    } else {
+      // повідомлення про помилку 
+    }
+  };
+
+  const handleDeleteCard = () => {
+    onDelete();
+  };
 
   return (
-    <div className="blog-card">
-      <h2>{name}</h2>
+    <div className={styles["common"]}>
+      <label>
+        Name:
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+      <label>
+        Text:
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+      </label>
+      <label>
+        Is Show:
+        <input type="checkbox" checked={isShow} onChange={() => setIsShow(!isShow)} />
+      </label>
+      <label>
+        Date Time Publish:
+        <input type="text" value={dateTimePublish} onChange={(e) => setDateTimePublish(e.target.value)} />
+      </label>
+
       {showDetails && (
         <>
+          <h2>{name}</h2>
           <p>{text}</p>
-          <img src={image} alt={name} />
           <p>{`Published on: ${dateTimePublish}`}</p>
         </>
       )}
-      <p>{`Show: ${isShow ? "Yes" : "No"}`}</p>
-      <Button variant="primary" onClick={onAdd}>
+     <Button variant="primary" onClick={handleAddCard}>
         Add Card
       </Button>
-      <Button variant="error" onClick={onDelete}>
+      <Button variant="error" onClick={handleDeleteCard}>
         Delete Card
       </Button>
       <Button variant="secondary" onClick={handleToggleDetails}>
@@ -34,3 +78,5 @@ const BlogCard = ({ name, text, image, isShow, dateTimePublish, onAdd, onDelete 
 
 
 export default BlogCard;
+
+
