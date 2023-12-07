989048a5cf94e795129a8f3d23d24b7d208f2d57
Elena <alena.stepanyuk@gmail.com>
2023-12-07 19:34:24 +0200

    added footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <Link to="/">2023</Link>
+      <Link to="/group">Group F-31</Link>
+      <Link to="/svg">Images</Link>
+    </div>
+  );
 };
 export default Footer;
