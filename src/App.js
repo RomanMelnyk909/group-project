799a885136340f89627281b3a2c963bb3bfa0915
unknown <sashabasiuk1908@gmail.com>
2023-12-09 10:20:24 +0200

    fixed hader and footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import PageWrapper from "./components/PageWrapper";
-import Logo from "./components/Logo";
 
 import { Routes, Route } from "react-router";
 import { ABOUT_PATH, BLOG_PATH, HOME_PATH } from "./constants/constants";
@@ -12,20 +10,12 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Navigation/>
-      <Logo />
       <Routes>
-        <Route path={CONTACT_PATH} element={CONTACT_PATH} />
         <Route path={HOME_PATH} element={HOME_PATH} />
         <Route path={ABOUT_PATH} element={ABOUT_PATH} />
         <Route path={BLOG_PATH} element={BLOG_PATH} />
-        <Route path={HELP_PATH} element={HELP_PATH} />
         {/* <Route path={COCTAIL_CATEGIRIES_PATH} element={``} /> */}
       </Routes>
-      <PageWrapper>
-        <Header />
-
-      </PageWrapper>
       <Footer />
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import Navigation from "../Navigation";
 import Logo from "../Logo";
 import "./header.css";
+
 const Header = () => {
 	return (
 		<div className="header">
 			<Logo />
 			<Navigation />
 		</div>
-	)
+	);
 };
 
 export default Header;
