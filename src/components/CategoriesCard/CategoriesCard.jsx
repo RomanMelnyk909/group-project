03438e200f56ff13a93ae62c6b6f7f9dca323cc1
valentine <yecvhuk.valentine@gmaiil.com>
2023-12-10 14:22:31 +0200

    Add_Category_Card

--- src/components/CategoriesCard/CategoriesCard.jsx
+++ src/components/CategoriesCard/CategoriesCard.jsx
@@ -1,13 +1,20 @@
 import styles from './categoriesCard.module.css'
 
+// Демо
+import backgroundImage from '../../images/sub-banner-1.jpg'
+
 
 const CategoriesCard = (props) => {
     const { title, image }= props;
 
     return (
-        <div className={styles['common']} style={{backgroundImage: '../../images/crunchy-whole-grain-chocolate-chip-cookies_1021.jpg'}}>
-            <h3>{title}</h3>
-            <img src={image} alt={`${image}`} />
+        // Коли картинка буде з api тодi замiнемо backgroundImage на props => image
+        <div className={styles['common']} style={{ backgroundImage: `url(${backgroundImage})` }}>
+            <h3>{title}The title will be here</h3>
+
+            {/* Демо */}
+            <a className={styles["demo-link"]} href="#">There will be a link here</a>
+            {/* <img src={image} alt={`${image}`} /> */}
         </div>
     )
 };
