1d835ecd1395359e4be5bef3e3a45015d94d6af5
DEV <dev@step.rivne>
2023-12-12 21:16:03 +0200

    added categories cards_intro

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,11 +1,50 @@
 import styles from './categories.module.css';
 import CategoriesCard from '../CategoriesCard';
+import { useEffect, useState } from 'react';
+import { CARTEGORIES_LIST_ENDPOINT } from '../../constants/endpoints';
+import { createRequestPath } from '../../helpers/helpers';
+import PageWrapper from '../PageWrapper'
+
 
 const Categories = () => {
+    const [data, setData]=useState([]);
+    const [fetching, setFetching]=useState(false);
+    const [fetchError, setFetchError] = useState(null);
+    console.log("data", data)
+
+    useEffect(() => {
+        setFetching(true);
+        fetch(createRequestPath(CARTEGORIES_LIST_ENDPOINT))
+        .then(response => response.json())
+        .then(resp => {
+            setData(resp)
+            setFetching(false)
+            console.log(resp)
+        })
+        .catch(err => {
+            setFetching(false)
+            setFetchError(err)
+        });
+    },[])
+
+
     return (
-        <div className={styles['common']}>
-            <CategoriesCard />
+        <PageWrapper>
+            <div className={styles['common']}>
+            {/* <CategoriesCard /> */}
+            {data.map((prod, id)=> {
+                const {title, image, priority } = prod;
+                return (
+                    <CategoriesCard
+                    title={title}
+                    image={image}
+                    // id={id}
+                    key={id}
+                    />
+                )
+            })}
         </div>
+        </PageWrapper>
     )
 };
 
--- src/components/CategoriesCard/CategoriesCard.jsx
+++ src/components/CategoriesCard/CategoriesCard.jsx
@@ -1,16 +1,21 @@
 import styles from './categoriesCard.module.css'
 
 // Демо
-import backgroundImage from '../../images/sub-banner-1.jpg'
+// import backgroundImage from '../../images/sub-banner-1.jpg'
+import backgroundImage from '../../images/sub-banner-2.jpg'
 
 
 const CategoriesCard = (props) => {
-    const { title, image }= props;
+    const { title, image, priority, id }= props;
+    // const imageUrl = image ? image : `url(${backgroundImage})`;
+    const imageUrl = image ? `url(${backgroundImage})` : image;
 
     return (
+
         // Коли картинка буде з api тодi замiнемо backgroundImage на props => image
-        <div className={styles['common']} style={{ backgroundImage: `url(${backgroundImage})` }}>
-            <h3>{title}The title will be here</h3>
+        <div className={styles['common']} style={{ backgroundImage: imageUrl }}>
+            {/* <h3>{title}The title will be here</h3> */}
+            <h3>{title}</h3>
 
             {/* Демо */}
             <a className={styles["demo-link"]} href="#">There will be a link here</a>
