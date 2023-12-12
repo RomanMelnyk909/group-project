7c25ab35e61be1d04e313603d62b45ad24be93d7
Elena <alena.stepanyuk@gmail.com>
2023-12-12 19:17:49 +0200

    fetched blog

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,28 @@
-
+import { useState, useEffect } from "react";
+import styles from "./blog.module.css";
+import { BASE_URL, BLOGS_LIST_ENDPOINT } from "../../constants/endpoints";
 
 const Blog = () => {
+  const [blogData, setBlogData] = useState(null);
 
-    return (
-        <div>Blog</div>
-    )
-}
+  const apiUrl = `${BASE_URL}${BLOGS_LIST_ENDPOINT}`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        setBlogData(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <div>Blog</div>;
+};
 
 export default Blog;
