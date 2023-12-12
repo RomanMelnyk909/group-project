de14464e69fb48340fb1f0ee826715f8e2564590
DEV <dev@step.rivne>
2023-12-12 19:11:43 +0200

    fetch products list

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,34 @@
-import styles from "./products.module.css"
+import styles from "./products.module.css";
+
+import { useEffect, useState } from "react";
+
+import { PRODUCTS_LIST_ENDPOINT } from "../../constants/endpoints";
+import { createRequestPath } from "../../helpers/helpers";
+
+
 
 const Products = () => {
+	const [data, setData] = useState([]);
+	const [fetching, setFetching] = useState(false);
+	const [error, setError] = useState(null);
+
+	useEffect(function(){
+		setFetching(true);
+		fetch(createRequestPath(PRODUCTS_LIST_ENDPOINT))
+		.then(response => response.json())
+		.then(resp => {
+			console.log(resp);
+			setFetching(false);
+			setData(resp);
+		})
+		.catch(err => {
+			console.log('err => ', err);
+			setFetching(false);
+			setError(err);
+		})
+	}, []);
+
+
 	return (
 		<div className={styles['products']}>Products</div>
 	)
