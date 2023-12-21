21e367c6fe1d6c3bf61a97d75dc388210a6a4079
DrobenyukA <drobenyuk.a@gmail.com>
2023-12-21 12:41:43 +0200

    fix blogCard

--- src/components/AddVladBlogs/AddVladBlogs.jsx
+++ src/components/AddVladBlogs/AddVladBlogs.jsx
@@ -1,51 +1,27 @@
-import React, { useState, useContext, useEffect } from "react";
-import Input from "../Input";
+import React, { useState } from "react";
 import { createRequestPath } from "../../helpers/helpers";
 import { BLOGS_ADD_ENDPOINT } from "../../constants/endpoints";
-import Blog from "../Blog"; 
+
 import PageWrapper from "../PageWrapper";
-import { v4 as uuidv4 } from 'uuid'; 
-import { ChangeIdContext } from "../../App";
 
-import styles from './addVladBlogs.module.css';
+import { BLOG_PATH } from "../../constants/pathNames";
 
-const AddVladBlogForm = () => {
-  let { refetchId, setRefetchId } = useContext(ChangeIdContext);
+import styles from "./addVladBlogs.module.css";
+import Button from "../Button";
+import { useNavigate } from "react-router";
 
-  const [name, setName] = useState('');
-  const [text, setText] = useState('');
-  const [image, setImage] = useState('');
+const AddVladBlogs = () => {
+  const navigator = useNavigate();
+
+  const [name, setName] = useState("");
+  const [text, setText] = useState("");
+  const [image, setImage] = useState("");
   const [isShow, setIsShow] = useState(true);
-  const [blogList, setBlogList] = useState([]);
-  const [dateTimePublish, setDateTimePublish] = useState('');
+  const [dateTimePublish, setDateTimePublish] = useState("");
 
-  useEffect(() => {
-    // Отримати дані з localStorage при завантаженні компонента
-    const storedBlogList = JSON.parse(localStorage.getItem('blogList')) || [];
-    setBlogList(storedBlogList);
-  }, []);
-
-  useEffect(() => {
-    // Зберегти дані у localStorage при зміні blogList
-    localStorage.setItem('blogList', JSON.stringify(blogList));
-  }, [blogList]);
-
-  const onSubmitDataToApi = (blog) => {
+  const onSubmitDataToApi = () => {
     const apiEndpoint = createRequestPath(BLOGS_ADD_ENDPOINT);
-    fetch(apiEndpoint, {
-      method: "POST",
-      body: JSON.stringify(blog),
-      headers: { "Content-Type": "application/json" },
-    })
-      .then(resp => {
-        console.log('response => ', resp);
-        if (resp.status) {
-          setRefetchId(uuidv4())	
-        }
-      })
-  }
 
-  const onAddBlog = () => {
     const newBlog = {
       name,
       text,
@@ -54,14 +30,16 @@ const AddVladBlogForm = () => {
       dateTimePublish,
     };
 
-    if (newBlog.name && newBlog.text && newBlog.image && newBlog.dateTimePublish) {
-      setBlogList((prevList) => [...prevList, newBlog]);
-      setName('');
-      setText('');
-      setImage('');
-      setDateTimePublish('');
+    if (name && text && image && dateTimePublish) {
+      fetch(apiEndpoint, {
+        method: "POST",
+        body: JSON.stringify(newBlog),
+        headers: { "Content-Type": "application/json" },
+      }).then((resp) => {
+        if (resp?.status === 200) navigator(BLOG_PATH);
+      });
     } else {
-      alert("Заповніть всі поля")
+      alert("Заповніть всі поля");
     }
   };
 
@@ -72,60 +50,50 @@ const AddVladBlogForm = () => {
           <div className={styles.blogPanel}>
             <label>
               Name
-              <input 
-                className={styles.list} 
-                placeholder="Name" 
-                onChange={(e) => setName(e.target.value)} 
-                value={name} 
+              <input
+                className={styles.list}
+                placeholder="Name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
               />
             </label>
             <label>
               Description
-              <input 
-                className={styles.list} 
-                placeholder="Enter some text" 
-                onChange={(e) => setText(e.target.value)} 
-                value={text} 
+              <input
+                className={styles.list}
+                placeholder="Enter some text"
+                onChange={(e) => setText(e.target.value)}
+                value={text}
               />
             </label>
           </div>
           <div className={styles.blogPanel}>
             <label>
               Image:
-              <input 
-                className={styles.list} 
-                placeholder="Image url" 
-                onChange={(e) => setImage(e.target.value)} 
-                value={image} 
+              <input
+                className={styles.list}
+                placeholder="Image url"
+                onChange={(e) => setImage(e.target.value)}
+                value={image}
               />
             </label>
             <label htmlFor="">
               Date Time Publish:
-              <input 
+              <input
                 type="date"
-                className={styles.list} 
-                placeholder="Publish date and time" 
-                onChange={(e) => setDateTimePublish(e.target.value)} 
-                value={dateTimePublish} 
+                className={styles.list}
+                placeholder="Publish date and time"
+                onChange={(e) => setDateTimePublish(e.target.value)}
+                value={dateTimePublish}
               />
             </label>
           </div>
         </div>
-        <button type="button" onClick={onAddBlog}>Add</button>
-        <hr />
-        {blogList.map((blog, index) => (
-          <div className={styles.card}>
-            <div key={index}>
-              <img src={"https://thelongfortgroup.com/public/img/default/no-image-icon.jpg"||blog.image  } alt="Blog" />
-              <h3>{blog.name}</h3>
-              <p>{blog.text}</p>
-              <p>Date Time Publish: {blog.dateTimePublish}</p>
-            </div>
-          </div>
-        ))}
+
+        <Button type="button" onClickFunction={onSubmitDataToApi} title="Add" />
       </div>
     </PageWrapper>
   );
-}
+};
 
-export default AddVladBlogForm;
+export default AddVladBlogs;
--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,122 +1,28 @@
-// BlogCard.js
+import { useNavigate } from "react-router";
 
-import React, { useState } from "react";
-import styles from "./blogcard.module.css";
+import Button from "../Button";
+import styles from "../Blog/blog.module.css";
 
-const BlogCard = ({
-  name: initialName,
-  text: initialText,
-  image: initialImage,
-  isShow: initialIsShow,
-  dateTimePublish: initialDateTimePublish,
-  onAdd,
-  onDelete,
-  index, // Додаємо індекс
-}) => {
-  const [name, setName] = useState(initialName);
-  const [text, setText] = useState(initialText);
-  const [image, setImage] = useState(initialImage);
-  const [isShow, setIsShow] = useState(initialIsShow);
-  const [dateTimePublish, setDateTimePublish] = useState(
-    initialDateTimePublish
-  );
-  const [showDetails, setShowDetails] = useState(false);
-  const [showForm, setShowForm] = useState(true);
-
-  const handleToggleDetails = () => {
-    setShowDetails(!showDetails);
-    setShowForm(false);
-  };
-
-  const handleAddCard = () => {
-    if (name && text && image && dateTimePublish) {
-      onAdd({ name, text, image, isShow, dateTimePublish });
-      setName("");
-      setText("");
-      setImage("");
-      setIsShow(true);
-      setDateTimePublish("");
-      setShowForm(true);
-    } else {
-      alert("Заповніть всі поля");
-    }
-  };
-
-  const handleDeleteCard = () => {
-    onDelete(index);
-  };
+const BlogCard = ({ id, name, text, dateTimePublish }) => {
+  const navigator = useNavigate();
 
   return (
-    <div className={styles.blogCard}>
-      {showForm && !initialName && (
-        <div className={styles.cardContent}>
-          <input
-            type="text"
-            placeholder="Name"
-            value={name}
-            className={styles.list}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-      )}
-      {showForm && !initialImage && (
-        <div className={styles.cardContent}>
-          <input
-            type="text"
-            placeholder="Image URL"
-            value={image}
-            className={styles.list}
-            onChange={(e) => setImage(e.target.value)}
-          />
-        </div>
-      )}
-      {showForm && !initialDateTimePublish && (
-        <div className={styles.cardContent}>
-          <input
-            type="date"
-            placeholder="Date Time Publish"
-            value={dateTimePublish}
-            className={styles.list}
-            onChange={(e) => setDateTimePublish(e.target.value)}
-          />
-        </div>
-      )}
-      {showForm && !initialText && (
-        <div className={styles.cardContent}>
-          <textarea
-            placeholder="Text"
-            value={text}
-            className={styles.list}
-            onChange={(e) => setText(e.target.value)}
-          ></textarea>
-        </div>
-      )}
-      <div className={styles.cardActions}>
-        {showForm && !initialName && (
-          <div className={styles.cardContent}>
-            <button onClick={handleAddCard}>Add Card</button>
-          </div>
-        )}
-        <button
-          className={styles.deleteBtn}
-          onClick={handleDeleteCard}
-          
-        >
-          Delete Card
-        </button>
-        <button type="submit" onClick={handleToggleDetails}>
-          {showDetails ? "Hide Details" : "Show Details"}
-        </button>
-      </div>
-      {showDetails && (
-        <>
-          {initialImage && <img src={initialImage} alt={initialName} />}
-          {initialText && <p>Text: {initialText}</p>}
-          {initialDateTimePublish && (
-            <p>Published on: {initialDateTimePublish}</p>
-          )}
-        </>
-      )}
+    <div key={id} className={styles.blogPost}>
+      <img
+        src="https://thelongfortgroup.com/public/img/default/no-image-icon.jpg"
+        alt=""
+      />
+      <h2>{name}</h2>
+      <p>{text}</p>
+      <p>Date: {dateTimePublish}</p>
+      <Button
+        type="button"
+        title="Edit"
+        onClickFunction={() => {
+          navigator(`/update-blog-r/${id}`);
+        }}
+      />
+      <Button type="reset"  title="Delete" />
     </div>
   );
 };
