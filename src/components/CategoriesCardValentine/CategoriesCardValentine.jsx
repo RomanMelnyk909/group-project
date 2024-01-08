85250956390e2b3676d39a13859b71b478e84a28
valentine <yecvhuk.valentine@gmaiil.com>
2024-01-08 11:02:12 +0200

    Added QueryLoader_Edit_Object deletion confirmation

--- src/components/CategoriesCardValentine/CategoriesCardValentine.jsx
+++ src/components/CategoriesCardValentine/CategoriesCardValentine.jsx
@@ -1,20 +1,54 @@
 import styles from './categoriesCardV.module.css'
 
-import { CARTEGORIES_DELETE_ENDPOINT } from '../../constants/endpoints';
+import Button from '../Button';
+import Input from '../Input';
+// import Modal from '../Modal/Modal';
+
+import { 
+    CARTEGORIES_DELETE_ENDPOINT, 
+    CARTEGORIES_EDITE_ENDPOINT 
+} from '../../constants/endpoints';
 import { createRequestPath } from '../../helpers/helpers';
+import { ChangeIdContext } from '../../App';
+
+import { useContext, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 // Демо
-// import backgroundImage from '../../images/sub-banner-1.jpg'
 import backgroundImage from '../../images/sub-banner-2.jpg'
+import { createPortal } from 'react-dom';
+// import QueryLoader from '../QueryLoader';
+
+// import { useNavigate } from 'react-router';
+// import { ADD_CATEGORIES_FORM_VALENTINE_PATH } from '../../constants/pathNames';
 
 
 
 const CategoriesCardValentine = (props) => {
-    // Олександр
-    // const { title, image, string, id} = props;
+    // const navigator = useNavigate();
+
     const { title, image,string, priority, urlSlug, id, onSetDeletedId }= props;
+
     // const imageUrl = image ? image : `url(${backgroundImage})`;
     const imageUrl = image ? `url(${backgroundImage})` : image;
+
+    const [ titleEdit, setTitleEdit ] = useState(title);
+    const [ imageEdit, setImageEdit ] = useState(image);
+    const [ priorityEdit, setPriorityEdit ] = useState(priority);
+    const [ urlSlugEdit, setUrlSlugEdit ] = useState(string);
+
+
+    const [ showModal, setShowModal ] = useState(false);
+    // const [ deleteCategory, setDeleteCategory ] = useState(false);
+    const [showFlag, setShowFlag] = useState(false);
+
+    let { setRefetchId } = useContext(ChangeIdContext);
+
+    const [ fetching, setFetching ] = useState(false)
+    const [ fetchError, setFetchError ] = useState(null);
+
+    let portalElement = document.querySelector('#portal')
+
 
     const onDeleteDataToApi = () => {
         const apiEndpoint = createRequestPath(CARTEGORIES_DELETE_ENDPOINT, id);
@@ -29,29 +63,196 @@ const CategoriesCardValentine = (props) => {
                 return resp;
             })
             .catch(err => console.log('error => ', err))
+            // .catch(err => {
+            //     setFetching(false)
+            //     setFetchError(err)
+            // })
     }
+    
+    function onEditDataToApi (category){
+        setFetching(true)
+        fetch(createRequestPath(CARTEGORIES_EDITE_ENDPOINT), {
+            method: "PUT",
+            body: JSON.stringify(category),
+            headers: { "Content-Type": "application/json" },
+        })
+        .then(resp => {
+            if (resp.status) {
+                setRefetchId(uuidv4())
+            }
+        })
+        .catch(err => console.log("ERROR =>", err))
+        // .catch(err => {
+        //     setFetching(false)
+        //     setFetchError(err)
+        // })
+    }
+
+    function onEditCategory () {
+        const category = {
+            id:id,
+			title:titleEdit,
+			image: imageEdit,
+			priority:priorityEdit,
+			urlSlug:urlSlugEdit,
+		};
+        onEditDataToApi(category);
+        setShowFlag(false);
+    }
+
+    const onGetName = (value) => {
+        setTitleEdit(value)
+    };
+
+    const onGetImage = (value) => {
+        setImageEdit(value)
+    }
+    const onGetPriority = (value) => {
+        setPriorityEdit(value)
+    }
+    const onGetUrlSlug = (value) => {
+        setUrlSlugEdit(value)
+    };
+
+
+    const onCancel = () => {
+		setShowFlag(false);
+		// setShowModal(false);
+        setShowModal(false)
+        // setDeleteCategory(false)
+	}
+	const onShow = () => {
+		setShowFlag(true);
+        // setDeleteCategory(true)
+	}
+
+	const onDelete = () => {
+		setShowModal(true);
+        // setDeleteCategory(true)
+	}
+
+    let modalContent = (
+        <div className={styles['delete']}>  
+            <h2>Are you sure you want to delete {title} ?</h2>
+            {/* <h2>Edite caregory</h2> */}
+                {/* <Input 
+                    label="Name " 
+                    placeholder="Enter category name" 
+                    onChangeFunction={onGetName} 
+                    value={titleEdit}
+                    validation={true} />
+                <Input 
+                    label="Image " 
+                    placeholder="Enter category image url" 
+                    onChangeFunction={onGetImage} 
+                    value={imageEdit}
+                    validation={true} />
+                <Input 
+                    label="Priority " 
+                    placeholder="Enter category priority" 
+                    onChangeFunction={onGetPriority} 
+                    type='number' 
+                    value={priorityEdit}
+                    validation={true} />
+                <Input 
+                    label="UrlSlug " 
+                    placeholder="Enter category urlSlug" 
+                    onChangeFunction={onGetUrlSlug} 
+                    value={urlSlugEdit}
+                    validation={true} />
+
+                <Button 
+                    type='button'
+                    title='Add changes'
+                    onClickFunction={onEditCategory}/>
+                <Button 
+                    type='button'
+                    title='Cancel'
+                    onClickFunction={onCancel}/> */}
+                <Button 
+                    type='reset'
+                    title='Delete'
+                    onClickFunction={onDeleteDataToApi}/>
+                <Button 
+                    type='button'
+                    title='Cancel'
+                    onClickFunction={onCancel}/>
+
+
+        </div>
+    )
 
     return (
 
         // Коли картинка буде з api тодi замiнемо backgroundImage на props => image
-        // Олександр
-        // <div className={styles['common']} style={{ backgroundImage: `url(${image || backgroundImage})` }}id={id}>
-
         // <div className={styles['common']} style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <>
+        {/* <QueryLoader fetching={fetching} error={fetchError}> */}
 
-        <div className={styles['common']} style={{ backgroundImage: imageUrl }}>
-            <h3>{title || `The title will be here`}</h3>
-            {/* <h3>{title}The title will be here</h3> */}
+            <div className={styles['common']} style={{ backgroundImage: imageUrl }}>
+                <h3>{title || `The title will be here`}</h3>
 
-            {/* Демо */}
+                {/* Демо */}
+                <a className={styles["demo-link"]} href="#">{string || "There will be a link here"}</a>
+                {/* <img src={image} alt={`${image}`} /> */}
+        
+                <Button 
+                    type="reset" 
+                    onClickFunction={onDelete} 
+                    title={"delete"}/>
+                <Button 
+                    // className={styles["demos"]} 
+                    type="button" 
+                    title={"edit"} 
+                    onClickFunction={onShow}/>
+                
 
-            {/* Олександр */}
-            {/* <a className={styles["demo-link"]} href="#">{string || `There will be a link here`}</a> */}
+                {/* <Modal title={title} deleteCategory={deleteCategory} setDeleteCategory={setDeleteCategory} onDeleteDataToApi={onDeleteDataToApi}/> */}
+            </div>
+            {/* </QueryLoader> */}
 
-            <a className={styles["demo-link"]} href="#">{string || "There will be a link here"}</a>
-            {/* <img src={image} alt={`${image}`} /> */}
-            <button className={styles["btn-card"]} onClick={onDeleteDataToApi}>delete</button>
-        </div>
+            <div className={showFlag?styles["edit"]:styles["hidden"]}>
+                <h2>Edite Category</h2>
+                <Input 
+                    label="Name " 
+                    placeholder="Enter category name" 
+                    onChangeFunction={onGetName} 
+                    value={titleEdit}
+                    validation={true} />
+                <Input 
+                    label="Image " 
+                    placeholder="Enter category image url" 
+                    onChangeFunction={onGetImage} 
+                    value={imageEdit}
+                    validation={true} />
+                <Input 
+                    label="Priority " 
+                    placeholder="Enter category priority" 
+                    onChangeFunction={onGetPriority} 
+                    type='number' 
+                    value={priorityEdit}
+                    validation={true} />
+                <Input 
+                    label="UrlSlug " 
+                    placeholder="Enter category urlSlug" 
+                    onChangeFunction={onGetUrlSlug} 
+                    value={urlSlugEdit}
+                    validation={true} />
+
+                <Button 
+                    type='button'
+                    title='Add changes'
+                    onClickFunction={onEditCategory}/>
+                <Button 
+                    type='button'
+                    title='Cancel'
+                    onClickFunction={onCancel}/>
+
+            </div>
+                {showModal ? createPortal(modalContent, portalElement) : null}
+
+       
+        </>
     )
 };
 
