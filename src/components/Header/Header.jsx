a02762ffd9d598ea75c80929cd2df79031c44496
DrobenyukA <drobenyuk.a@gmail.com>
2024-01-17 12:50:36 +0200

    add_redux_vlad

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,42 @@ import Logo from "../Logo";
 import "./header.css";
 
 import PageWrapper from "../PageWrapper";
+import { useDispatch } from "react-redux";
+import { DECERMENT, DOUBLE_CARD_COUNT, INCERMENT, PLUS_NUMBER, RESET } from "../../constants/actions";
+import { useState } from "react";
 const Header = () => {
+
+	const [number, setNumber] = useState(0)
+
+	const dispatch = useDispatch();
+
+	// const onPlusHandler = () => {
+	// 	dispatch({ type: INCERMENT })
+	// }
+	// const onMinusHandler = () => {
+	// 	dispatch({ type: DECERMENT })
+	// }
+	// const onResetHandler = () => {
+	// 	dispatch({ type: RESET })
+	// }
+	// const onPlusNumberHandler = () => {
+	// 	dispatch({ type:PLUS_NUMBER, payload:number })
+	// }
+const handleDoubleCardCount = () => {
+    dispatch({ type: DOUBLE_CARD_COUNT });
+  };
+
 	return (
 		<div className="header">
 			<PageWrapper>
 				<div className="head">
-				<Logo />
-				<Navigation />
+					<button onClick={handleDoubleCardCount}>*</button>
+      				{/* <button onClick={onMinusHandler}>-</button> */}
+					{/* <input type="text" onChange={(e) => setNumber(+e.target.value)} /> */}
+      				{/* <button onClick={onPlusNumberHandler}>plus number</button> */}
+      				{/* <button onClick={onResetHandler }>reset</button> */}
+					<Logo />
+					<Navigation />
 				</div>
 			</PageWrapper>
 		</div>
